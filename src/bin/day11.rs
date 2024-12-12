bef99db0c91c57bdcb97cd19fc12e99eb94428d2
Gregory Presser <gpress2222@gmail.com>
2024-12-12 09:40:13 -0500

    did day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::collections::HashMap;
 
 const PUZZLE_INPUT: &'static str = include_str!("input/day11.txt");
 
@@ -15,86 +15,44 @@ fn count_digits(num: usize) -> usize {
     }
 }
 
-struct VecSizeOrd<T>(Vec<T>);
-
-impl<T> Ord for VecSizeOrd<T> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.0.len().cmp(&other.0.len())
+fn rec(val: usize, dur: usize, size: usize, dp: &mut HashMap<(usize, usize), usize>) -> usize {
+    if dur == 0 {
+        return size;
     }
-}
 
-impl<T> PartialOrd for VecSizeOrd<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.0.len().partial_cmp(&other.0.len())
+    if let Some(count) = dp.get(&(val, dur)) {
+        return *count;
     }
-}
 
-impl<T> PartialEq for VecSizeOrd<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.len() == other.0.len()
+    if val == 0 {
+        let res = rec(1, dur - 1, size, dp);
+        dp.insert((val, dur), res);
+        return res;
     }
-}
+    let digit_len = count_digits(val);
 
-impl<T> Eq for VecSizeOrd<T> {}
+    if digit_len % 2 == 0 {
+        let val_str = format!("{}", val);
+        let (first, second) = val_str.split_at(digit_len / 2);
+        let first = first.parse().unwrap();
+        let second = second.parse().unwrap();
+        let res = rec(first, dur - 1, size, dp) + rec(second, dur - 1, size, dp);
+        dp.insert((val, dur), res);
+        return res;
+    }
+    let res = rec(val * 2024, dur - 1, size, dp);
+
+    dp.insert((val, dur), res);
+    return res;
+}
 
 fn puzzle(input: &str, size: usize) -> usize {
-    let mut lst: Vec<usize> = input
+    let mut dp: HashMap<(usize, usize), usize> = HashMap::new();
+    input
         .trim()
         .split_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
-
-    let mut dp: BinaryHeap<VecSizeOrd<usize>> = BinaryHeap::new();
-    for generation in 0..size {
-        println!("got to {}/{} len = {}", generation, size - 1, lst.len());
-
-        let mut new_lst: Vec<usize> = vec![];
-
-        'outer: for i in 0..lst.len() {
-            for VecSizeOrd(c) in dp.iter() {
-                // 3 >= 5 - 1
-                if c.len() >= lst.len() - i {
-                    // make sure the sublist is long enough
-                    continue;
-                }
-
-                let sublst = &lst[i..(i + c.len())];
-
-                assert_eq!(
-                    sublst.len(),
-                    c.len(),
-                    "sublist len: {}, c len: {}, lst len: {}, i: {}",
-                    sublst.len(),
-                    c.len(),
-                    lst.len(),
-                    i
-                );
-
-                if c.iter().zip(sublst.iter()).all(|(a, b)| a == b) {
-                    new_lst.extend(c.iter());
-                    println!("found: skipping {} els", c.len());
-                    break 'outer;
-                }
-            }
-
-            let x = lst[i];
-            if x == 0 {
-                new_lst.push(1);
-                // return vec![1].into_iter();
-            } else if count_digits(x) % 2 == 0 {
-                let xstr = format!("{x}");
-                let (a, b) = xstr.split_at(count_digits(x) / 2);
-                new_lst.push(a.parse().unwrap());
-                new_lst.push(b.parse().unwrap());
-            } else {
-                new_lst.push(x * 2024);
-            }
-        }
-
-        lst = new_lst;
-        dp.push(VecSizeOrd(lst.clone()));
-    }
-    lst.len()
+        .map(|x| rec(x.parse().unwrap(), size, 1, &mut dp))
+        .sum()
 }
 
 #[cfg(test)]
